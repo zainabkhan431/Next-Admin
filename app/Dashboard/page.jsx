7fcd26da61de2f0fb971d0b963789f5aeb445b1b
Zainab Khan <zainabfarooq431@gmail.com>
2025-02-27 23:08:41 +0500

    commit

--- app/Dashboard/page.jsx
+++ app/Dashboard/page.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import Card from "../ui/dashboard/card/card";
+import Chart from "../ui/dashboard/chart/chart";
+import styles from "../ui/dashboard/dashboard.module.css";
+import Rightbar from "../ui/dashboard/rightbar/rightbar";
+import Transactions from "../ui/dashboard/transaction/transaction.jsx";
 
-export default function Dashboard() {
+const Dashboard = () => {
   return (
-    <div>
-      Dashboard
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          {/* {Card.map((item) => (
+            <Card item={item} key={item.id} />
+          ))} */}
+          <Card/>
+          <Card/>
+          <Card/>
+        </div>
+        <Transactions />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <Rightbar />
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Dashboard;
---
+++ app/ui/dashboard/transaction/transaction.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import styles from "./transaction.module.css";
+export default function transaction() {
+  return (
+    <div className={styles.container}>
+<h2> Latest Transaction</h2>
+<table className={styles.table}>
+  <thead>
+    <tr>
+      <td>Name</td>
+      <td>Status</td>
+      <td>Date</td>
+      <td>Amount</td>
+    </tr>
+  </thead>
+  <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+              <img
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.pending}`}>
+                Pending
+              </span>
+            </td>
+            <td>14.02.2024</td>
+            <td>$3.200</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+              <img
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.done}`}>Done</span>
+            </td>
+            <td>14.02.2024</td>
+            <td>$3.200</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+              <img
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.cancelled}`}>
+                Cancelled
+              </span>
+            </td>
+            <td>14.02.2024</td>
+            <td>$3.200</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <img
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.pending}`}>
+                Pending
+              </span>
+            </td>
+            <td>14.02.2024</td>
+            <td>$3.200</td>
+          </tr>
+        </tbody>
+
+</table>
+    </div>
+  )
+}
